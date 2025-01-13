849cbe85fc4fe9967927de76b887ab9cc6e5a2e3
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-01-13 20:24:01 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,9 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     # change this to your ssh key
-    "CHANGE"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPwZL27kGTQDIlSe03abT9F24nSAizORyjo5cI3BD92s your_email@example.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOJ6+aLTPanIYS88EjCVtCZv6pw2jC4lIIZNRY6VrnoF pungkula@laptop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLU9Ri6EVsKMHMXm1L5N0sU9qUVrQDgmC+o6vJnik9u pungis@nasty"
   ];
 
   system.stateVersion = "24.05";
